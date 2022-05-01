87120a6b989c8e8767937fadc91589bcad0ce88d
PhatPhamFe <phatphamm@usumsoftware.com>
2022-05-01 17:21:49 +0700

    init

--- src/templates/config.template.ts
+++ src/templates/config.template.ts
@@ -1,13 +1,5 @@
 export const configTemplate = (config: string): string => {
   return `
-// @ts-check
-// Simple-graphql-to-typescript configuration file, see link for more information
-// https://sgts.netlify.com/configuration/config.html
-
-
-/**
- * @type { import("simple-graphql-to-typescript").SgtsConfig }
- */
 ${config}
 `;
 };
