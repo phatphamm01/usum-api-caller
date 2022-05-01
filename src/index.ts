298fcb3f5352fd4ecf286f5694532bb89de1b13c
PhatPhamFe <phatphamm@usumsoftware.com>
2022-05-01 15:45:00 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ async function startApolloServer(typeDefs, resolvers) {
   await server.start();
   server.applyMiddleware({ app });
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 4000;
 
   await new Promise<void>((resolve) =>
     httpServer.listen({ port: PORT }, resolve)
