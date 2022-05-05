049305895eb0ce0bd41f2e37e52df3df587ae2ae
PhatPhamFe <phatphamm@usumsoftware.com>
2022-05-05 10:29:52 +0700

    fix fragmet

--- src/generators/templates.generator.ts
+++ src/generators/templates.generator.ts
@@ -34,7 +34,7 @@ export function generateTemplateQuery({
         ${methodName}${hasArgs ? `(${GQLArgs.join(',')})` : ''} {
           ${innerFragment}
         }
-      } \${isFragment ? fragment : ''}
+      } \${isFragment ? queryBuilder(fragment) : ''}
       \``;
   } else {
     return `gql\`
